b3963ac650338c9c6074a76cb209f9439ef1caa0
Azazadeh <azadeh.nourbakhsh@gmail.com>
2023-02-06 21:34:10 +0300

    added apiKey and url

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+function showTemp(response) {
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.city;
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+}
 
+let apiKey = "39e003bc66b7f35722e0c7a5dt3o9140";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Tehran&key=39e003bc66b7f35722e0c7a5dt3o9140&units=metric`;
+
+axios.get(apiUrl).then(showTemp);
