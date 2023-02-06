208cb60bed7b8eaf0c46534250f5a7232617dba4
Azazadeh <azadeh.nourbakhsh@gmail.com>
2023-02-07 00:09:47 +0300

    added current temp details and some edits in html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 function showTemp(response) {
   let city = document.querySelector("#city");
-  city.innerHTML = response.data.city;
   let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = Math.round(response.data.temperature.current);
-}
+  let currentCondition = document.querySelector("#current-condition");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
 
+  city.innerHTML = response.data.city;
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  currentCondition.innerHTML = response.data.condition.description;
+  humidity.innerHTML = response.data.temperature.humidity;
+  wind.innerHTML = response.data.wind.speed;
+
+  console.log(response.data);
+}
 let apiKey = "39e003bc66b7f35722e0c7a5dt3o9140";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Tehran&key=39e003bc66b7f35722e0c7a5dt3o9140&units=metric`;
 
