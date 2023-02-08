9454ecba10775182eeb03c63e57879de99724dd7
Azazadeh <azadeh.nourbakhsh@gmail.com>
2023-02-08 17:17:45 +0300

    added Time (seems buggy!! probably change it later)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,53 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function showTemp(response) {
   let city = document.querySelector("#city");
   let temperature = document.querySelector("#temperature");
   let currentCondition = document.querySelector("#current-condition");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
+  let date = document.querySelector("#date");
 
   city.innerHTML = response.data.city;
   temperature.innerHTML = Math.round(response.data.temperature.current);
   currentCondition.innerHTML = response.data.condition.description;
   humidity.innerHTML = response.data.temperature.humidity;
   wind.innerHTML = response.data.wind.speed;
+  date.innerHTML = formatDate(response.data.time * 1000);
 
   console.log(response.data);
 }
 let apiKey = "39e003bc66b7f35722e0c7a5dt3o9140";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Tehran&key=39e003bc66b7f35722e0c7a5dt3o9140&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Seoul&key=39e003bc66b7f35722e0c7a5dt3o9140&units=metric`;
 
 axios.get(apiUrl).then(showTemp);
